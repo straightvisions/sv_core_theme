e83f1540de8a64e92e9176787979604f6e44e2cc
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-06-28 10:49:37 +0200

    Updated version number

--- core_theme.php
+++ core_theme.php
@@ -13,8 +13,8 @@ namespace sv100;
 require_once( 'core/core.php' );
 
 class init extends \sv_core\core {
-	const version 						= 4000;
-	const version_core_match 			= 4000;
+	const version 						= 4001;
+	const version_core_match 			= 4001;
 	
 	public static $is_child_theme 		= false;
 	private $modules_registered 		= array();
