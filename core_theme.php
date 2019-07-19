803589035112a7fdd3ddd257b371414ee4ce4fae
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-19 14:12:35 +0200

    SUB - UPDATE - Core

--- core_theme.php
+++ core_theme.php
@@ -13,8 +13,8 @@ namespace sv100;
 require_once( 'core/core.php' );
 
 class init extends \sv_core\core {
-	const version 						= 4108;
-	const version_core_match 			= 4012;
+	const version 						= 4109;
+	const version_core_match 			= 4013;
 	
 	public static $is_child_theme 		= false;
 	private $modules_registered 		= array();
