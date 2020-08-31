4c259b5398a7d4e5d68e6ceb5ddd3de7e4adb6ef
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-08-31 10:26:50 +0200

    UPDATE CORE

--- core_theme.php
+++ core_theme.php
@@ -13,8 +13,8 @@ namespace sv100;
 require_once( 'core/core.php' );
 
 class init extends \sv_core\core {
-	const version 								= 1435; // should match version in style.css and readme.txt
-	const version_core_match 					= 4030;
+	const version 								= 1436; // should match version in style.css and readme.txt
+	const version_core_match 					= 4031;
 	
 	public static $is_child_theme 				= false;
 	private $modules_registered 				= array();
